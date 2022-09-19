9478c8dc9be8ea059c9d7c1788285f4a3b9e3ff9
darianhang <darianhang@gmail.com>
2022-09-19 19:07:47 -0400

    update

--- api/server.js
+++ api/server.js
@@ -1,22 +1,28 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require("path");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(express.static(path.resolve(__dirname, "./client/build")));
 
 MongoDB = "mongodb+srv://durianhang:durianadmin@cluster0.vqcxcdk.mongodb.net/?retryWrites=true&w=majority"
 
 mongoose.connect(process.env.MONGODB_URI || MongoDB, {
 	useNewUrlParser: true, 
 	useUnifiedTopology: true 
-}).then(() => console.log("Connected to MongoDB")).catch(console.error);
+}).then(() => console.log(`Connected to MongoDB on ${process.env.PORT || 3001}`)).catch(console.error);
 
 // Models
 const BidItem = require('./models/Bid-Item');
 const Bidder = require('./models/Bidders');
+
+app.get("*", function (request, response) {
+	response.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+  });
 
 app.get('/BidItems', async (req, res) => {
 	const item = await BidItem.find();
