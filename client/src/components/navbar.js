0b49905d009d6549102123213f103d52c6a1aeba
darianhang <darianhang@gmail.com>
2022-09-20 12:28:31 -0400

    add nav-bar animation

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,9 +1,18 @@
 import React from "react"
+import { motion, AnimatePresence } from "framer-motion"
 
 export default function NavBar() {
     return (
-        <div className="nav-bar">
-            <img className="nav-img" src="https://res.cloudinary.com/df5etts2d/image/upload/v1663618941/Untitled-1_pmjsvt.png"></img>
-        </div>
+        <AnimatePresence>
+            <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ delay: 1.5, duration: 1 }}
+            >
+                <div className="nav-bar">
+                    <img className="nav-img" src="https://res.cloudinary.com/df5etts2d/image/upload/v1663618941/Untitled-1_pmjsvt.png"></img>
+                </div>
+            </motion.div>
+        </AnimatePresence>
     )
 }
