ee0990088e540761f7e50c4253b8aee4faf23269
darianhang <82900199+darianhang@users.noreply.github.com>
2022-10-05 12:01:53 -0400

    Temp Takedown

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <h2>Auction Over</h2>
   </React.StrictMode>,
   document.getElementById('root')
 );
