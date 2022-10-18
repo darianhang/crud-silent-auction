db643b33e5f0e2914ca4491bf01854a789bed184
darianhang <82900199+darianhang@users.noreply.github.com>
2022-10-18 19:51:04 -0400

    Update index.js
    Removed temporary landing page.

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <h2>Auction Over</h2>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
